96fcb61ff4ba66c2aaf8ff36c1aeaf653fb4b301
Ciya Silva <ciyasilva1278@gmail.com>
2024-01-03 09:19:44 +0530

    first commit

---
+++ scool/src/scool/B1class122.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package scool;
+
+/**
+ * @author bca
+ *
+ */
+public abstract class B1class122 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
