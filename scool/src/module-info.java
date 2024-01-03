e029be38a5189c81990d4237f570c8ca089017f3
crystal060804 <crystalbarreto008@gmail.com>
2024-01-03 09:17:46 +0530

    Change-Id: I0000000000000000000000000000000000000000
    Change-Id: I75079b92dbfe97d882dde18482bb36ad1caa88c4

---
+++ scool/src/module-info.java
@@ -0,0 +1,2 @@
+module scool {
+}
---
+++ scool/src/scool/hello.java
@@ -0,0 +1,11 @@
+package scool;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Bye.");
+	}
+
+}
